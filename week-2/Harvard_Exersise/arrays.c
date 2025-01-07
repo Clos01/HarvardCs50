87414e396c788b6d35a780f09aa5a05305193af0
Carlos Rivas <Cjrivas00@gmail.com>
2025-01-06 22:26:24 -0500

    used a const and made sure no floating numbers

---
+++ week-2/Harvard_Exersise/arrays.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int const SIZE = 5;
+int main()
+{
+    //Declare array
+    int numbers[SIZE];
+    numbers[0] = 1;
+
+for(int i =1; i < SIZE; i++)
+{
+numbers[i] = numbers[i-1] * 2;
+
+
+}
+    // int numbers[5] = {1, 2, 4, 8, 16};
+
+    //Print out each element in the array
+    
+    for (int i =0; i < 5; i++)
+    {
+        printf("%d\n", numbers[i]);
+    }
+
+
+
+return 0;
+}
