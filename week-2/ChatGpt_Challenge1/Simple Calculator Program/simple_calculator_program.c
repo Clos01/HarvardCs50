d3370e89a9630be79f8de2de8a2720a0c547b2a2
Carlos Rivas <Cjrivas00@gmail.com>
2025-01-11 20:44:13 -0500

    added functions

--- week-2/ChatGpt_Challenge1/Simple Calculator Program/simple_calculator_program.c
+++ week-2/ChatGpt_Challenge1/Simple Calculator Program/simple_calculator_program.c
@@ -5,6 +5,9 @@ void displayMenu();
 int getUserChoice();
 double add();
 double subtract();
+double getNumber(const char *prompt);
+double performDivision(double num1, double num2);
+
 double Division();
 double multiplication();
 int main()
@@ -21,11 +24,14 @@ int main()
     }
     else if (userChoice == 3)
     {
+
+        // double getNumber(const char* prompt);
+        // double performDivision(double num1, double num2);
         Division();
     }
     else if (userChoice == 4)
     {
-       multiplication();
+        multiplication();
     }
     return 0;
 }
@@ -87,15 +93,42 @@ double subtract()
     return results;
 }
 
+double getNumber(const char *prompt)
+{
+    double number;
+    printf("%s", prompt);
+    scanf("%lf", &number);
+    return number;
+}
+
+double performDivision(double num1, double num2)
+{
+    if (num2 == 0)
+    {
+        printf("Error: Division by Zero!\n");
+        return 0;
+    }
+    else
+    {
+        return num1 / num2;
+    }
+}
+
 double Division()
 {
-    double num1, num2;
-    printf("What number do you choose to Division: \n");
-    scanf("%lf", &num1);
-    printf("What number do you choose to Division: \n");
-    scanf("%lf", &num2);
-    double results = num1 / num2;
-    printf("results: %lf", results);
+
+    double num1 = getNumber("Enter the First Number: ");
+    double num2 = getNumber("Enter the second Number: ");
+    double results = performDivision(num1, num2);
+    if (results == 0)
+    {
+        printf("Ignore results: %lf", results);
+    }
+    else
+    {
+        printf("results: %lf", results);
+    }
+
     return results;
 }
 
