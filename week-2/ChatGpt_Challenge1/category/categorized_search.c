7456147eb219975d2fda61c520823dd452a7de2d
Carlos Rivas <Cjrivas00@gmail.com>
2025-01-16 21:15:04 -0500

    fixed issues for taking user input intially

--- week-2/ChatGpt_Challenge1/category/categorized_search.c
+++ week-2/ChatGpt_Challenge1/category/categorized_search.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 #include <string.h>
 
-char* displayMessage(char usrInput[]);
+// Function declarations
+void displayMessage(char usrInput[]);
 char* displayMessageLogic(char usrInput[]);
 int main()
 {
+    //declaring an array to store user input 
 char displayingMessage[100];
+//Call functions
 displayMessage( displayingMessage );
 displayMessageLogic (displayingMessage);
 
 
 return 0;
 }
-
-char* displayMessage(char usrInput[])
-{
-     printf("\n");
+void displayMessage(char usrInput[]) {
+    printf("\n");
     printf("What are you searching for: \n");
-     printf("\n");
-    scanf("%s", usrInput);
-
+    printf("\n");
+    scanf("%s", usrInput); // Read input from the user
 }
+
 
 
 //check logic for displayMessage
 
-char* displayMessageLogic(char usrInput)
+char* displayMessageLogic(char usrInput[])
 {
  printf("You entered: %s\n", usrInput);  
- scanf("%s". usrInput);
 }
